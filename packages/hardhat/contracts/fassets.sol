ce185bce13dc76d3d2edfe6b629b55f6af4879f7
elitebook <matus.gavornik@gmail.com>
2025-05-30 23:20:08 +0200

    backed token mint creation

---
+++ packages/hardhat/contracts/fassets.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title Rizz Token - An ERC20 token with configurable supply
+/// @author Matus Gavornik
+/// @notice This contract creates an ERC20 token with supply based on parameters
+/// @dev Supply is calculated as number * multiplicator * 10^18
+contract Rizz is ERC20 {
+    uint private constant _decimals = 18;
+    constructor(uint256 number, uint256 multiplicator) ERC20("RizzToken", "RIZZ") {
+        uint256 totalSupply = CalculateTotalSuply(number, multiplicator);
+        _mint(msg.sender, totalSupply);
+    }
+
+    function CalculateTotalSuply(uint256 number, uint256 multiplicator) internal pure returns (uint256) {
+        uint256 rawSupply = number * multiplicator;
+        uint256 totalSupply = rawSupply * 10**_decimals;
+        return totalSupply;
+    }
+    function decimals() public view virtual override returns (uint8) {
+        return uint8(_decimals);
+    }
+
+}
